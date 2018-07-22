ee65c508116a2ee22cd9b5b41ef5e0fa06ceaf86
zsly3n3 <zsly3n@163.com>
2018-07-22 18:42:48 +0800

    add

--- src/components/insert.vue
+++ src/components/insert.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div>
     <select v-model="ppselected">
         <option v-for="item in types" :key="item.id" :value="item.id">
           {{item.type}}
@@ -21,10 +21,30 @@
       format="yyyy 年 MM 月 dd 日"
       value-format="timestamp">
     </el-date-picker>
-
+<input type="file" @change="importFile(this)" id="imFile" style="display: none" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/>
+  <el-button class="button" @click="uploadFile()">导入</el-button>
+  <!--错误信息提示-->
+  <el-dialog title="提示" v-model="errorDialog" size="tiny">
+<span>{{errorMsg}}</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button type="primary" @click="errorDialog=false">确认</el-button>
+  </span>
+  </el-dialog>
+  <!--展示导入信息-->
+  <el-table :data="excelData" tooltip-effect="dark" v-loading="loading"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)">
+<el-table-column label="名称" prop="name" show-overflow-tooltip></el-table-column>
+<el-table-column label="分量" prop="size" show-overflow-tooltip></el-table-column>
+<el-table-column label="口味" prop="taste" show-overflow-tooltip></el-table-column>
+<el-table-column label="单价(元)" prop="price" show-overflow-tooltip></el-table-column>
+<el-table-column label="剩余(份)" prop="remain" show-overflow-tooltip></el-table-column>
+  </el-table>
   </div>
 </template>
 <script>
+var XLSX = require('xlsx')
 export default {
   name: 'insert',
   data () {
@@ -64,11 +84,26 @@ export default {
           }
         ]
       },
-      value7: ''
+      value7: '',
+      imFile: '', // 导入文件el
+      errorDialog: false, // 错误信息弹窗
+      errorMsg: '', // 错误信息内容
+      loading: false,
+      excelData: [
+        {
+          name: '红烧鱼', size: '大', taste: '微辣', price: '40', remain: '100'
+        },
+        {
+          name: '麻辣小龙虾', size: '大', taste: '麻辣', price: '138', remain: '200'
+        }
+      ]
     }
   },
   created: function () {
     this.$emit('isHideNav', false)
+  },
+  mounted: function () {
+    this.imFile = document.getElementById('imFile')
   },
   activated: function () {
     this.$emit('switchroute', '/')
@@ -77,6 +112,64 @@ export default {
     test: function () {
       console.log(this.value7[0])
       console.log(this.value7[1])
+    },
+    arrToJsonString: function (obj) {
+      console.log(JSON.stringify(obj))
+    },
+    uploadFile: function () {
+      this.imFile.click()
+    },
+    importFile: function () { // 导入excel
+      this.loading = true
+      let obj = this.imFile
+      if (!obj.files) {
+        this.loading = false
+        return
+      }
+      var f = obj.files[0]
+      var reader = new FileReader()
+      let $t = this
+      reader.onload = function (e) {
+        var data = e.target.result
+        if ($t.rABS) {
+          $t.wb = XLSX.read(btoa(this.fixdata(data)), {type: 'base64'})
+        } else {
+          $t.wb = XLSX.read(data, {type: 'binary'})
+        }
+        let json = XLSX.utils.sheet_to_json($t.wb.Sheets[$t.wb.SheetNames[0]])
+        console.log(typeof json)
+        $t.dealFile($t.analyzeData(json)) // analyzeData: 解析导入数据
+      }
+      if (this.rABS) {
+        reader.readAsArrayBuffer(f)
+      } else {
+        reader.readAsBinaryString(f)
+      }
+    },
+    analyzeData: function (data) {
+      return data
+    },
+    dealFile: function (data) {
+      console.log(data)
+      this.imFile.value = ''
+      this.loading = false
+      if (data.length <= 0) {
+        this.errorDialog = true
+        this.errorMsg = '请导入正确信息'
+      } else {
+        this.excelData = data
+        console.log(this.arrToJsonString(data))
+      }
+    },
+    fixdata: function (data) {
+      var o = ''
+      var l = 0
+      var w = 10240
+      for (; l < data.byteLength / w; ++l) {
+        o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)))
+      }
+      o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)))
+      return o
     }
   }
 }
@@ -84,4 +177,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.el-table th>.cell {
+  text-align: center;
+}
+.button {
+  margin-bottom: 20px;
+}
 </style>
